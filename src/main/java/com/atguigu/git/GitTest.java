563412d04ba0c85a7e8bbdedffdb5ea540f1db34
ioeah <1525476571@qq.com>
2023-05-14 21:24:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author ioeah
+ * @version 1.0
+ * @date 2023/5/14 21:20
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+    }
+}
